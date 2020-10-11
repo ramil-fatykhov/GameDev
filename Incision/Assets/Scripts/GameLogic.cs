403ae41540b54058d769830780abb5abdd456128
ramil-fatykhov <ramikr@live.ru>
2020-10-11 19:02:35 +0300

    Finished

--- Incision/Assets/Scripts/GameLogic.cs
+++ Incision/Assets/Scripts/GameLogic.cs
@@ -12,6 +12,7 @@ public class GameLogic : MonoBehaviour {
     public Button spot_3;
     public Button spot_4;
     public Button spot_5;
+    public Button spot_6;
     public GameObject check;
 
     public Button spot_1_pressed;
@@ -19,15 +20,30 @@ public class GameLogic : MonoBehaviour {
     public Button spot_3_pressed;
     public Button spot_4_pressed;
     public Button spot_5_pressed;
+    public Button spot_6_pressed;
     public GameObject check_win;
     public GameObject check_lose;
 
-    private bool pressed_spot_1;
-    private bool pressed_spot_2;
-    private bool pressed_spot_3;
-    private bool pressed_spot_4;
-    private bool pressed_spot_5;
-    private bool pressed_check;
+    static private bool pressed_spot_1;
+    static private bool pressed_spot_2;
+    static private bool pressed_spot_3;
+    static private bool pressed_spot_4;
+    static private bool pressed_spot_5;
+    static private bool pressed_spot_6;
+    static private bool pressed_check;
+
+    public bool isHard;
+
+    public bool firstLvl1;
+    public bool firstLvl2;
+    public bool firstLvl3;
+    public bool firstLvl4;
+    public bool SecondLvl1;
+    public bool SecondLvl2;
+    public bool SecondLvl3;
+    public bool SecondLvl4;
+
+    static private bool CheckAnswerParam;
 
     public GameObject winningPanel;
     public GameObject losingPanel;
@@ -113,6 +129,21 @@ public class GameLogic : MonoBehaviour {
         spot_5_pressed.gameObject.SetActive(false);
     }
 
+    public void press_6_spot()
+    {
+        pressed_spot_6 = true;
+        spot_6.gameObject.SetActive(false);
+        spot_6_pressed.gameObject.SetActive(true);
+    }
+
+
+    public void visible_6_spot()
+    {
+        pressed_spot_6 = false;
+        spot_6.gameObject.SetActive(true);
+        spot_6_pressed.gameObject.SetActive(false);
+    }
+
     IEnumerator WaitForTry()
     {
         yield return new WaitForSeconds(1.0f);
@@ -126,11 +157,13 @@ public class GameLogic : MonoBehaviour {
         spot_3.interactable = true;
         spot_4.interactable = true;
         spot_5.interactable = true;
+        spot_6.interactable = true;
         spot_1_pressed.interactable = true;
         spot_2_pressed.interactable = true;
         spot_3_pressed.interactable = true;
         spot_4_pressed.interactable = true;
         spot_5_pressed.interactable = true;
+        spot_6_pressed.interactable = true;
 
         if (!_isLose && (HealthController.count != 0 || TimerController.currentTime != 0f))
         {
@@ -169,6 +202,17 @@ public class GameLogic : MonoBehaviour {
         {
             button_index_5 = UnityEngine.Random.Range(1, 6);
         }
+            int button_index_6 = UnityEngine.Random.Range(1, 7);
+            if (isHard)
+            {
+                while ((button_index_6 == button_index_4) || (button_index_6 == button_index_3)
+               || (button_index_6 == button_index_2) || (button_index_6 == button_index_1)
+               || (button_index_6 == button_index_5))
+            {
+                button_index_6 = UnityEngine.Random.Range(1, 7);
+            }
+        }
+        
 
         //add rotation end
 
@@ -177,24 +221,38 @@ public class GameLogic : MonoBehaviour {
         spot_3.transform.position = getButtonCoordinates(button_index_3);
         spot_4.transform.position = getButtonCoordinates(button_index_4);
         spot_5.transform.position = getButtonCoordinates(button_index_5);
+        if (isHard)
+        {
+           spot_6.transform.position = getButtonCoordinates(button_index_6);
+        }
 
         spot_1.transform.rotation = Quaternion.Euler(getButtonRotation(button_index_1));
         spot_2.transform.rotation = Quaternion.Euler(getButtonRotation(button_index_2));
         spot_3.transform.rotation = Quaternion.Euler(getButtonRotation(button_index_3));
         spot_4.transform.rotation = Quaternion.Euler(getButtonRotation(button_index_4));
         spot_5.transform.rotation = Quaternion.Euler(getButtonRotation(button_index_5));
-
+        if (isHard)
+        {
+            spot_6.transform.rotation = Quaternion.Euler(getButtonRotation(button_index_6));
+        }
         spot_1_pressed.transform.position = spot_1.transform.position;
         spot_2_pressed.transform.position = spot_2.transform.position;
         spot_3_pressed.transform.position = spot_3.transform.position;
         spot_4_pressed.transform.position = spot_4.transform.position;
         spot_5_pressed.transform.position = spot_5.transform.position;
-
+        if (isHard)
+        {
+            spot_6_pressed.transform.position = spot_6.transform.position;
+        }
         spot_1_pressed.transform.rotation = spot_1.transform.rotation;
         spot_2_pressed.transform.rotation = spot_2.transform.rotation;
         spot_3_pressed.transform.rotation = spot_3.transform.rotation;
         spot_4_pressed.transform.rotation = spot_4.transform.rotation;
         spot_5_pressed.transform.rotation = spot_5.transform.rotation;
+        if (isHard)
+        {
+            spot_6_pressed.transform.rotation = spot_6.transform.rotation;
+        }
         //init spots buttons end
     }
 
@@ -202,23 +260,68 @@ public class GameLogic : MonoBehaviour {
     {
         if (i == 1)
         {
-            return new Vector3(Globals.EASY_SPOT_X_POSITION_1, Globals.EASY_SPOT_Y_POSITION_1, 0);
+            if (isHard)
+            {
+                return new Vector3(Globals.HARD_SPOT_X_POSITION_1, Globals.HARD_SPOT_Y_POSITION_1, 0);
+            }
+            else
+            {
+                return new Vector3(Globals.EASY_SPOT_X_POSITION_1, Globals.EASY_SPOT_Y_POSITION_1, 0);
+            }
         }
         else if (i == 2)
         {
-            return new Vector3(Globals.EASY_SPOT_X_POSITION_2, Globals.EASY_SPOT_Y_POSITION_2, 0);
+            if (isHard)
+            {
+                return new Vector3(Globals.HARD_SPOT_X_POSITION_2, Globals.HARD_SPOT_Y_POSITION_2, 0);
+            }
+            else
+            {
+                return new Vector3(Globals.EASY_SPOT_X_POSITION_2, Globals.EASY_SPOT_Y_POSITION_2, 0);
+            }
         }
         else if (i == 3)
         {
-            return new Vector3(Globals.EASY_SPOT_X_POSITION_3, Globals.EASY_SPOT_Y_POSITION_3, 0);
+            if (isHard)
+            {
+                return new Vector3(Globals.HARD_SPOT_X_POSITION_3, Globals.HARD_SPOT_Y_POSITION_3, 0);
+            }
+            else
+            {
+                return new Vector3(Globals.EASY_SPOT_X_POSITION_3, Globals.EASY_SPOT_Y_POSITION_3, 0);
+            }
         }
         else if (i == 4)
         {
-            return new Vector3(Globals.EASY_SPOT_X_POSITION_4, Globals.EASY_SPOT_Y_POSITION_4, 0);
+            if (isHard)
+            {
+                return new Vector3(Globals.HARD_SPOT_X_POSITION_4, Globals.HARD_SPOT_Y_POSITION_4, 0);
+            }
+            else
+            {
+                return new Vector3(Globals.EASY_SPOT_X_POSITION_4, Globals.EASY_SPOT_Y_POSITION_4, 0);
+            }
         }
-        else
+        else if (i == 5)
         {
-            return new Vector3(Globals.EASY_SPOT_X_POSITION_5, Globals.EASY_SPOT_Y_POSITION_5, 0);
+            if (isHard)
+            {
+                return new Vector3(Globals.HARD_SPOT_X_POSITION_5, Globals.HARD_SPOT_Y_POSITION_5, 0);
+            }
+            else
+            {
+                return new Vector3(Globals.EASY_SPOT_X_POSITION_5, Globals.EASY_SPOT_Y_POSITION_5, 0);
+            }
+        } else
+        {
+            if (isHard)
+            {
+                return new Vector3(Globals.HARD_SPOT_X_POSITION_6, Globals.HARD_SPOT_Y_POSITION_6, 0);
+            }
+            else
+            {
+                return new Vector3(Globals.EASY_SPOT_X_POSITION_6, Globals.EASY_SPOT_Y_POSITION_6, 0);
+            }
         }
     }
 
@@ -226,23 +329,62 @@ public class GameLogic : MonoBehaviour {
     {
         if (i == 1)
         {
-            return new Vector3(0, 0, 0);
+            if (isHard)
+            {
+                return new Vector3(0, 0, 0);
+            } else
+            {
+                return new Vector3(0, 0, 0);
+            }
         }
         else if (i == 2)
         {
-            return new Vector3(0, 0, 70);
+            if (isHard)
+            {
+                return new Vector3(0, 0, 61);
+            } else
+            {
+                return new Vector3(0, 0, 73);
+            }
         }
         else if (i == 3)
         {
-            return new Vector3(0, 0, 140);
+            if (isHard)
+            {
+                return new Vector3(0, 0, 121);
+            } else
+            {
+                return new Vector3(0, 0, 145);
+            }
         }
         else if (i == 4)
         {
-            return new Vector3(0, 0, 211);
+            if (isHard)
+            {
+                return new Vector3(0, 0, 181);
+            } else
+            {
+                return new Vector3(0, 0, 217);
+            }
         }
-        else
+        else if (i == 5)
         {
-            return new Vector3(0, 0, 286);
+            if (isHard)
+            {
+                return new Vector3(0, 0, 241);
+            } else
+            {
+                return new Vector3(0, 0, 289);
+            }
+        } else
+        {
+            if (isHard)
+            {
+                return new Vector3(0, 0, 301);
+            } else
+            {
+                return new Vector3(0, 0, 289);
+            }
         }
     }
 
@@ -254,13 +396,48 @@ public class GameLogic : MonoBehaviour {
         spot_3.interactable = false;
         spot_4.interactable = false;
         spot_5.interactable = false;
+        if (isHard)
+        {
+            spot_6.interactable = false;
+        }
+
         spot_1_pressed.interactable = false;
         spot_2_pressed.interactable = false;
         spot_3_pressed.interactable = false;
         spot_4_pressed.interactable = false;
         spot_5_pressed.interactable = false;
+        if (isHard)
+        {
+            spot_6_pressed.interactable = false;
+        }
 
-        if (pressed_check && ((pressed_spot_1 && pressed_spot_2 && !pressed_spot_3 && !pressed_spot_4 && !pressed_spot_5) || ((!pressed_spot_1 && !pressed_spot_2 && pressed_spot_3 && pressed_spot_4) || pressed_spot_5)))
+        if (firstLvl1)
+        {
+            CheckAnswerParam = (pressed_spot_1 || pressed_spot_4) && (pressed_spot_2 || pressed_spot_3);
+        } else if (firstLvl2)
+        {
+            CheckAnswerParam = (pressed_spot_5 || (pressed_spot_1 && pressed_spot_2 && pressed_spot_3));
+        } else if (firstLvl3)
+        {
+            CheckAnswerParam = (pressed_spot_3 || pressed_spot_4);
+        } else if (firstLvl4)
+        {
+            CheckAnswerParam = (((pressed_spot_1 || pressed_spot_3 || pressed_spot_4) && pressed_spot_2 && pressed_spot_5) || ((pressed_spot_1 && pressed_spot_3) || (pressed_spot_1 && pressed_spot_4) || (pressed_spot_3 && pressed_spot_4)));
+        } else if (SecondLvl1)
+        {
+            CheckAnswerParam = (((pressed_spot_1 || pressed_spot_3 || pressed_spot_4) && pressed_spot_2 && pressed_spot_5) || ((pressed_spot_1 && pressed_spot_3) || (pressed_spot_1 && pressed_spot_4) || (pressed_spot_3 && pressed_spot_4)));
+        } else if (SecondLvl2)
+        {
+            CheckAnswerParam = ((pressed_spot_1 && (pressed_spot_5 || pressed_spot_4) && (pressed_spot_6 || pressed_spot_2)) || ((pressed_spot_6 || pressed_spot_2) && pressed_spot_3 && (pressed_spot_5 || pressed_spot_4)));
+        } else if (SecondLvl3)
+        {
+            CheckAnswerParam = ((pressed_spot_1 && pressed_spot_2) || (pressed_spot_3 && pressed_spot_4 && pressed_spot_5));
+        } else if (SecondLvl4)
+        {
+            CheckAnswerParam = (pressed_spot_1 && (((pressed_spot_2 || pressed_spot_6) && pressed_spot_3) || pressed_spot_4) || (pressed_spot_5 && (((pressed_spot_2 || pressed_spot_6) && pressed_spot_3) || pressed_spot_4)));
+        }
+
+        if (pressed_check && CheckAnswerParam)
         {
             check.gameObject.SetActive(false);
             check_win.gameObject.SetActive(true);
@@ -308,6 +485,7 @@ public class GameLogic : MonoBehaviour {
         pressed_spot_3 = false;
         pressed_spot_4 = false;
         pressed_spot_5 = false;
+        pressed_spot_6 = false;
         pressed_check = false;
 
         _sceneController = gameObject.AddComponent<SceneController>();
@@ -325,7 +503,7 @@ public class GameLogic : MonoBehaviour {
     // Update is called once per frame
     void Update()
     {
-        if (pressed_spot_1 || pressed_spot_2 || pressed_spot_3 || pressed_spot_4 || pressed_spot_5)
+        if (pressed_spot_1 || pressed_spot_2 || pressed_spot_3 || pressed_spot_4 || pressed_spot_5 || pressed_spot_6)
         {
             check.gameObject.SetActive(true);
         }
--- Incision/Assets/Scripts/Globals.cs
+++ Incision/Assets/Scripts/Globals.cs
@@ -4,27 +4,44 @@ using UnityEngine;
 
 public class Globals : MonoBehaviour
 {
-    public static int CANVAS_X = 575;
-    public static int CANVAS_Y = 266;
+    public static int CANVAS_X = 540;
+    public static int CANVAS_Y = 960;
 
     //spots button
-    public static int EASY_SPOT_X_POSITION_1 = -9 + CANVAS_X;
-    public static int EASY_SPOT_Y_POSITION_1 = -115 + CANVAS_Y;
+    public static int EASY_SPOT_X_POSITION_1 = 3 + CANVAS_X;
+    public static int EASY_SPOT_Y_POSITION_1 = -200 + CANVAS_Y;
 
-    public static int EASY_SPOT_X_POSITION_2 = 80 + CANVAS_X;
-    public static int EASY_SPOT_Y_POSITION_2 = -60 + CANVAS_Y;
+    public static int EASY_SPOT_X_POSITION_2 = 175 + CANVAS_X;
+    public static int EASY_SPOT_Y_POSITION_2 = -70 + CANVAS_Y;
 
-    public static int EASY_SPOT_X_POSITION_3 = 51 + CANVAS_X;
-    public static int EASY_SPOT_Y_POSITION_3 = 33 + CANVAS_Y;
+    public static int EASY_SPOT_X_POSITION_3 = 100 + CANVAS_X;
+    public static int EASY_SPOT_Y_POSITION_3 = 135 + CANVAS_Y;
 
-    public static int EASY_SPOT_X_POSITION_4 = -53 + CANVAS_X;
-    public static int EASY_SPOT_Y_POSITION_4 = 36 + CANVAS_Y;
+    public static int EASY_SPOT_X_POSITION_4 = -120 + CANVAS_X;
+    public static int EASY_SPOT_Y_POSITION_4 = 135 + CANVAS_Y;
 
-    public static int EASY_SPOT_X_POSITION_5 = -90 + CANVAS_X;
-    public static int EASY_SPOT_Y_POSITION_5 = -57 + CANVAS_Y;
+    public static int EASY_SPOT_X_POSITION_5 = -180 + CANVAS_X;
+    public static int EASY_SPOT_Y_POSITION_5 = -75 + CANVAS_Y;
 
-    public static int EASY_SPOT_X_POSITION_6 = 340 + CANVAS_X;
-    public static int EASY_SPOT_Y_POSITION_6 = -500 + CANVAS_Y;
+    public static int EASY_SPOT_X_POSITION_6 = -160 + CANVAS_X;
+    public static int EASY_SPOT_Y_POSITION_6 = -110 + CANVAS_Y;
 
     // end spots button
+    public static int HARD_SPOT_X_POSITION_1 = 3 + CANVAS_X;
+    public static int HARD_SPOT_Y_POSITION_1 = -200 + CANVAS_Y;
+
+    public static int HARD_SPOT_X_POSITION_2 = 160 + CANVAS_X;
+    public static int HARD_SPOT_Y_POSITION_2 = -100 + CANVAS_Y;
+
+    public static int HARD_SPOT_X_POSITION_3 = 160 + CANVAS_X;
+    public static int HARD_SPOT_Y_POSITION_3 = 90 + CANVAS_Y;
+
+    public static int HARD_SPOT_X_POSITION_4 = 3 + CANVAS_X;
+    public static int HARD_SPOT_Y_POSITION_4 = 175 + CANVAS_Y;
+
+    public static int HARD_SPOT_X_POSITION_5 = -160 + CANVAS_X;
+    public static int HARD_SPOT_Y_POSITION_5 = 80 + CANVAS_Y;
+
+    public static int HARD_SPOT_X_POSITION_6 = -160 + CANVAS_X;
+    public static int HARD_SPOT_Y_POSITION_6 = -110 + CANVAS_Y;
 }
